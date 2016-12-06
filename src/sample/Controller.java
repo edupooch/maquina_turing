91eaf9d3971c4d748e5657a1d500cb7c06d43c67
edupooch <edupooch@gmail.com>
2016-12-06 12:31:32 -0200

    visao controle

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,30 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+    private static final String SEPARADOR = ",";
+
+    @FXML
+    private TextField tfAlfabetoEnt;
+    @FXML
+    private TextField tfEstados;
+    @FXML
+    private TextField tfEstadosFinais;
+    @FXML
+    private TextField tfSimbolosEsp;
+    @FXML
+    private GridPane grid;
 
     public void gerarTabela(){
+        String[] entradas = tfAlfabetoEnt.getText().split(SEPARADOR);
+        String[] estados = tfEstados.getText().split(SEPARADOR);
+        String[] estadosFinais = tfEstadosFinais.getText().split(SEPARADOR);
+        String[] simbolosEspeciais = tfSimbolosEsp.getText().split(SEPARADOR);
+
+
 
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,28 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.controlsfx.control.spreadsheet.GridBase;
+import org.controlsfx.control.spreadsheet.SpreadsheetCell;
+import org.controlsfx.control.spreadsheet.SpreadsheetCellType;
+import org.controlsfx.control.spreadsheet.SpreadsheetView;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
+
     }
 
 
